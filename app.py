04512bcbdcb30d28cdc4cda32896d30849aa2de0
Raul Prior <1715110187@utec-tgo.edu.mx>
2017-03-23 11:00:36 -0500

    add field image

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import web
 import model
 
 urls = (
-    '/index', 'Index',
+    '/', 'Index',
     '/see/(\d+)', 'See',
     '/edit/(\d+)', 'Edit',
     '/add', 'Add',
@@ -38,8 +38,25 @@ class Add:
 		return render.add()
 	def POST(self):
 		form = web.input()
-		model.add_product(form.product, form.description, form.stock, form.cost_price, form.sale_price)
-		raise web.seeother('/index')
+		imagen = web.input(image_product={})
+		filedir = 'static/images'#indica la ruta en la que se va a guardar
+		#Las siguientes dos lineas se evitan si se crea un nombre para el archivo
+		filepath = imagen.image_product.filename.replace('\\','/')#Elimina la ruta absoluta
+		filename = filepath.split('/')[-1]
+		#Copiar el archivo al servidor
+		fileserver = open(filedir + '/' + filename, 'w')
+		fileserver.write(imagen.image_product.file.read())
+		fileserver.close()
+
+		image_product = filename
+		model.add_product(form['product'],
+			form['description'],
+			form['stock'],
+			form['cost_price'],
+			form['sale_price'],
+			image_product
+			)
+		raise web.seeother('/')
 
 class Delete:
 	def GET(self, id_product):
@@ -49,7 +66,7 @@ class Delete:
 	def POST(self, id_product):
 		form = web.input()
 		model.delete_product(form.id)
-		raise web.seeother('/index')
+		raise web.seeother('/')
 
 
 if __name__ == "__main__":
--- model.py
+++ model.py
@@ -1,9 +1,15 @@
 import web
 
 db_host = 'y0nkiij6humroewt.cbetxkdyhwsb.us-east-1.rds.amazonaws.com'#host
-db_name = 'gpw5td5hlz65mrqa'#nombre de la base de datos
 db_user = 'whhb7hzets64lglk'#usuario
 db_passworld = 'u9to7skhda094huz'#contrasena
+db_name = 'gpw5td5hlz65mrqa'#nombre de la base de datos
+
+#db_host = 'localhost'#host
+#db_name = 'products'#nombre de la base de datos
+#db_user = 'root'#usuario
+#db_passworld = '1234'#contrasena
+
 
 db = web.database(#Metodo de conexion
 		dbn = 'mysql',#Tipo de servidor
@@ -26,10 +32,23 @@ def get_product(id):
 		return e
 
 def update_product(id, product, description, stok, cost_price, sale_price):
-	    db.update('products', where="id_product=$id", vars=locals(), product=product, description=description, stok=stok, cost_price=cost_price, sale_price=sale_price)
+	    db.update('products',
+	    	where="id_product=$id", 
+	    	vars=locals(), 
+	    	product=product, 
+	    	description=description, 
+	    	stok=stok, 
+	    	cost_price=cost_price, 
+	    	sale_price=sale_price)
 
-def add_product(product, description, stok, cost_price, sale_price):
-    db.insert('products', product=product, description=description, stok=stok, cost_price=cost_price, sale_price=sale_price)
+def add_product(product, description, stock, cost_price, sale_price, image):
+    db.insert('products',
+    	product=product, 
+    	description=description, 
+    	stok=stock,
+    	cost_price=cost_price,
+    	sale_price=sale_price,
+    	image_product=image)
 
 def delete_product(id):
 	db.delete('products', where='id_product=$id', vars=locals())
